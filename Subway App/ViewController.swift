06f1190dcb8fa47f47dcba419e1ba019942bc197
lucth98 <lucas@thiess.info>
2022-12-19 16:52:24 +0100

    Initial Commit

---
+++ Subway App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subway App
+//
+//  Created by Lucas on 19.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
