ec014b2974d4b0c461d41bbecd13991154eecd84
lucth98 <lucas@thiess.info>
2022-12-28 18:18:29 +0100

    test

--- Subway App/ViewControllers/ShowSubwayMapView.swift
+++ Subway App/ViewControllers/ShowSubwayMapView.swift
@@ -13,7 +13,7 @@ class ShowSubwayMapView: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        //test
         
         
     }
