7d4b9fc961977107c7b305d462cbc2d84dace42d
lucth98 <lucas@thiess.info>
2023-01-02 18:16:28 +0100

    display gps location

--- Subway App/ViewControllers/NearestStationView.swift
+++ Subway App/ViewControllers/NearestStationView.swift
@@ -8,10 +8,13 @@
 import Foundation
 import UIKit
 import MapKit
+import CoreLocation
 
-class NearestStationView: UIViewController {
+class NearestStationView: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,63 @@ class NearestStationView: UIViewController {
         
         mapView.mapType = MKMapType.hybrid
         
+        
+        
+        //location
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        
+        locationManager?.requestAlwaysAuthorization()
+        
+        
+        locationManager?.requestLocation()
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let lastLocation = locations.last
+        print("")
+        print("cordinatene:")
+        print(lastLocation?.coordinate.latitude)
+        print(lastLocation?.coordinate.longitude)
+        
+        var latitude: Double? = lastLocation?.coordinate.latitude
+        var longitude: Double? = lastLocation?.coordinate.longitude
+        
+        if(latitude != nil && longitude != nil){
+            displayGPSPosition(latitude!, longitude!)
+        }
+        
+        
+    }
+    
+    func  displayGPSPosition(_ latitude: Double, _ longitude: Double ){
+        
+        let cordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        //MKPlacemark(coordinate: <#T##CLLocationCoordinate2D#>, addressDictionary: <#T##[String : Any]?#>)
+        let place: MKPlacemark = MKPlacemark(coordinate: cordinate,addressDictionary: ["String" : "Any"])
+        
+        
+        
+        
+        mapView.addAnnotation(place)
+        
+        mapView.setCenter(cordinate, animated: false)
+    }
+    
+    
+    
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print("")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error:")
+        print(error)
         
     }
 
